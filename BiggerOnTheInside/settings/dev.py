56772786585428aec048e89f97d07694a08f4dba
Haythem Mansour <haythemmansour@Haythems-MacBook-Pro.local>
2023-04-25 22:34:39 -0600

    ugh

--- BiggerOnTheInside/settings/dev.py
+++ BiggerOnTheInside/settings/dev.py
@@ -5,6 +5,7 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-rzqpltar+2ki1f2n=4oxwranltflpzi8x#5n$u)fo6f^7q)a1r"
 
+#what the heck man
 
 DATABASES = {
     "default": {
