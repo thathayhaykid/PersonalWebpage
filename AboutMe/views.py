614a71a92b9c7bbcd579428ddc4aafae9ee8c15b
Haythem Mansour <haythemmansour@Haythems-MacBook-Pro.local>
2023-04-25 23:51:41 -0600

    trying

--- AboutMe/views.py
+++ AboutMe/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    x=1
-    x=2
     return render(request, 'hello.html', {'name' : 'Haythem'})
     #return render(request, 'hello.html', {'name' : 'Haythem'})
 
