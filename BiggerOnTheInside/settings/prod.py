a8a0bfe6b729db41d804bce2cc0e641aa13bd435
Haythem M <hayhaymansour@gmail.com>
2023-05-06 02:17:25 -0600

    More stuff

--- BiggerOnTheInside/settings/prod.py
+++ BiggerOnTheInside/settings/prod.py
@@ -2,14 +2,24 @@ from .common import *
 import dj_database_url
 import os
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'haymansour.herokuapp.com'
 ]
 
-SECRET_KEY = os.environ['HAYWEBSITE_SECRET_KEY']
+if DEBUG is True:
+    SECRET_KEY = SECRET_KEY = "django-insecure-rzqpltar+2ki1f2n=4oxwranltflpzi8x#5n$u)fo6f^7q)a1r"
+else:
+    SECRET_KEY = os.environ['HAYWEBSITE_SECRET_KEY']
 
-DATABASES = {
-    "default": dj_database_url.config()
-}
+if DEBUG is True:
+    DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "BiggerOnTheInside",
+        "HOST": "localhost",
+        "USER": "root",
+        "PASSWORD": "hayhay12!" }}
+else:
+        DATABASES = { "default": dj_database_url.config() }
