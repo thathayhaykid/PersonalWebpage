08e967ea6b22df9ad5f92a03796bc52f209dedcc
Haythem M <hayhaymansour@gmail.com>
2023-05-08 13:06:41 -0600

    stuff

--- AboutMe/urls.py
+++ AboutMe/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 #URLConfModule
 urlpatterns = [
-    path('hello/', views.say_hello)
+    path('hello/', views.say_hello),
+    path('HeyTala/', views.heytala)
 ]
--- AboutMe/views.py
+++ AboutMe/views.py
@@ -5,5 +5,7 @@ def say_hello(request):
     return render(request, 'hello.html', {'name' : 'Haythem'})
     #return render(request, 'hello.html', {'name' : 'Haythem'})
 
+def heytala(request):
+    return render(request,'HeyTala.html')
 
 # Create your views here.
